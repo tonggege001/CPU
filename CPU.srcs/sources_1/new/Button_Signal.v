7121a053578f31222c276bb11a3310aa50633578
tonggege <wontun12@gmail.com>
2019-02-19 09:17:39 +0800

    按钮防抖动

---
+++ CPU.srcs/sources_1/new/Button_Signal.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+module Button_Signal(Clk,original_button,new_button);
+    input Clk;
+    input original_button;
+    output reg new_button;
+
+	reg button_temp;
+
+	initial begin
+		button_temp <= 0;
+	end
+
+	always @(posedge clk) begin
+		button_temp <= original_button;
+		new_button <= (~button_temp) & original_button;
+	end
+
+endmodule
