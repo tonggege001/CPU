68914e103b1d6ff1dfd4ad4e4524bc04aa777ac1
yangzurong <1241935175@qq.com>
2019-02-20 08:18:23 +0800

    上传仿真文件‘

--- CPU.sim/sim_1/behav/compile.bat
+++ CPU.sim/sim_1/behav/compile.bat
@@ -1,7 +1,7 @@
 @echo off
-set xv_path=D:\\software\\VIVADO\\Vivado\\2015.2\\bin
-echo "xvlog -m64 --relax -prj Mem_Ins_vlog.prj"
-call %xv_path%/xvlog  -m64 --relax -prj Mem_Ins_vlog.prj -log compile.log
+set xv_path=C:\\Users\\pc\\Desktop\\Vivado\\2015.2\\bin
+echo "xvlog -m64 --relax -prj tb_Controler_vlog.prj"
+call %xv_path%/xvlog  -m64 --relax -prj tb_Controler_vlog.prj -log compile.log
 if "%errorlevel%"=="1" goto END
 if "%errorlevel%"=="0" goto SUCCESS
 :END
--- CPU.srcs/sim_1/new/tb_Controler.v
+++ CPU.srcs/sim_1/new/tb_Controler.v
@@ -1,33 +1,11 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2019/02/19 14:50:43
-// Design Name: 
-// Module Name: tb_Controler
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module tb_Controler();
-
-	 wire[5:0] OP;
-	 wire[5:0] Func;
-	 wire[3:0]AluOp;
+module tb_Controler;
+	reg [5:0]OP;
+	reg [5:0]Func;
+	wire [3:0]AluOp;
 	wire  MemtoReg;
 	wire  MemWrite;
-	wire   AluSrc;
+	wire  AluSrc;
 	wire  RegWrite;
 	wire  Syscall;
 	wire  SignedExt;
@@ -37,37 +15,38 @@ module tb_Controler();
 	wire  JR;
 	wire  JMP;
 	wire  JAL;
- Controller T6(OP, Func, AluOp,  MemtoReg , MemWrite, AluSrc, RegWrite, Syscall, SignedExt, RegDst , BEQ, BNE, JR, JMP, JAL);
-     always #5 clk = ~clk; 
-     initial begin
-     AluOp = 0;
-      RegWrite = 0;RegDst= 0;
-      MemtoReg = 0;MemWrite = 0;AluSrc = 0;Syscall = 0;SignedExt = 0;
-      BEQ = 0;BNE = 0;JR = 0;JMP = 0;JAL = 0;
-      #10  OP = 0 ; Func=0;   //SLL
-      #10  OP = 0 ; Func=3;    //SRA
-      #10  OP = 0 ; Func=2;    //SRL
-      #10  OP = 0 ; Func=32;    //ADD
-      #10  OP = 0 ; Func=33;     //ADDU
-      #10  OP = 0 ; Func=34;      //SUB
-      #10  OP = 0 ; Func=36;      //SUB
-      #10  OP = 0 ; Func=37;       //OR
-      #10  OP = 0 ; Func=39;       //NOR
-      #10  OP = 0 ; Func=42;       //SLT
-      #10  OP = 0 ; Func=43;       //SLTU
-      #10  OP = 0 ; Func=8;        //JR
-      #10  OP = 0 ; Func=12;       //SYSCALL
-      #10  OP = 2 ; Func=0;       //J
-      #10  OP = 3 ; Func=0;       //JAL
-      #10  OP = 4 ; Func=0;      //BEQ
-      #10  OP = 5 ; Func=43;      //BNE
-      #10  OP = 8 ; Func=8;        //ADDI
-      #10  OP =12 ; Func=0;      //ANDI
-      #10  OP = 9 ; Func=0;       //ANDI
-      #10  OP = 10 ; Func=0;       //SLTI
-      #10  OP = 13 ; Func=0;      //ORI
-      #10  OP = 35 ; Func=0;       //LW
-      #10  OP = 43 ; Func=0;      //SW
-   end
-      
+    Controller T6(OP, Func, AluOp,  MemtoReg , MemWrite, AluSrc, RegWrite, Syscall, SignedExt, RegDst , BEQ, BNE, JR, JMP, JAL);
+    //always #5 clk = ~clk; 
+    initial begin
+    //∏≥≥ı÷µ
+        AluOp <= 0;
+        RegWrite <= 0;RegDst <= 0;
+        MemtoReg <= 0;MemWrite <= 0;AluSrc <= 0;Syscall <= 0;SignedExt <= 0;
+        BEQ <= 0;BNE <= 0;JR <= 0;JMP <= 0;JAL <= 0;
+        
+        #10  OP = 0 ; Func=0;   //SLL
+        #10  OP = 0 ; Func=3;    //SR
+        #10  OP = 0 ; Func=2;    //SRL
+        #10  OP = 0 ; Func=32;    //ADD
+        #10  OP = 0 ; Func=33;     //ADDU
+        #10  OP = 0 ; Func=34;      //SUB
+        #10  OP = 0 ; Func=36;      //SUB
+        #10  OP = 0 ; Func=37;       //OR
+        #10  OP = 0 ; Func=39;       //NOR
+        #10  OP = 0 ; Func=42;       //SLT
+        #10  OP = 0 ; Func=43;       //SLTU
+        #10  OP = 0 ; Func=8;        //JR
+        #10  OP = 0 ; Func=12;       //SYSCALL
+        #10  OP = 2 ; Func=0;       //J
+        #10  OP = 3 ; Func=0;       //JAL
+        #10  OP = 4 ; Func=0;      //BEQ
+        #10  OP = 5 ; Func=43;      //BNE
+        #10  OP = 8 ; Func=8;        //ADDI
+        #10  OP =12 ; Func=0;      //ANDI
+        #10  OP = 9 ; Func=0;       //ANDI
+        #10  OP = 10 ; Func=0;       //SLTI
+        #10  OP = 13 ; Func=0;      //ORI
+        #10  OP = 35 ; Func=0;       //LW
+        #10  OP = 43 ; Func=0;      //SW
+    end    
 endmodule
